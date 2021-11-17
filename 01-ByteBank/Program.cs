99464d91b097689edb2d9ced2826a1a292213861
Gabriel Vieira <gabrielvieira1@protonmail.com>
2021-11-17 11:54:23 -0300

    Lesson 2, Default Values, Reference Type and Value Type

--- 01-ByteBank/Program.cs
+++ 01-ByteBank/Program.cs
@@ -10,7 +10,28 @@ namespace _01_ByteBank
   {
     static void Main(string[] args)
     {
-      new ContaCorrente();
+      ContaCorrente conta = new ContaCorrente();
+      conta.titular = "Isabela";
+      conta.agencia = 847;
+      conta.numero = 8476270;
+
+      ContaCorrente primeiraContaCorrente = new ContaCorrente();
+      primeiraContaCorrente.saldo = 200;
+      Console.WriteLine(primeiraContaCorrente.saldo);
+
+      primeiraContaCorrente.saldo += 100;
+      Console.WriteLine(primeiraContaCorrente.saldo);
+
+      ContaCorrente segundaContaCorrente = new ContaCorrente();
+      segundaContaCorrente.saldo = 50;
+
+      Console.WriteLine("primeira conta tem " + primeiraContaCorrente.saldo);
+      Console.WriteLine("segunda conta tem " + segundaContaCorrente.saldo);
+
+      primeiraContaCorrente = segundaContaCorrente;
+      Console.WriteLine("mesma referência conta tem " + primeiraContaCorrente.saldo);
+
+      Console.ReadLine();
 
       Console.ReadLine();
     }
